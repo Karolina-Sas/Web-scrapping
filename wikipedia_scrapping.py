bbf23dc322c1ef4831fd62a8afda668bc8c6de5e
Karolina Sas <karolina.sas@yahoo.com>
2022-05-07 22:37:45 +0200

    parser change

--- wikipedia_scrapping.py
+++ wikipedia_scrapping.py
@@ -7,25 +7,25 @@ start_time=time.time()
 
 url="https://pl.wikipedia.org/wiki/Kategoria:Polskie_zespo%C5%82y_punkrockowe"
 page=requests.get(url)
-soup=BeautifulSoup(page.content,"html.parser")
+soup=BeautifulSoup(page.content,"lxml")
 
 counter=0
 for a in soup.find_all("a",title=True, accesskey=False):    
     if a["href"][0:5]=="/wiki" and a["href"][0:16]!="/wiki/Wikipedia:" and a["href"][0:16]!="/wiki/Kategoria:":
             
-        with open('punk.txt', 'a+') as file:
+        with open('punk2.txt', 'a+') as file:
                 with redirect_stdout(file):                                      
                     print(a.get_text())                    
                     counter+=1
 file.close()
 
-with open('punk.txt', 'r') as file :
+with open('punk2.txt', 'r') as file :
   filedata = file.read()
 filedata = filedata.replace('(zespół muzyczny)', '')
 filedata = filedata.replace('(polski zespół muzyczny)', '')
 filedata = filedata.replace('(zespół)', '')
 
-with open('punk.txt', 'w') as file2:
+with open('punk2.txt', 'w') as file2:
   file2.write(filedata)
 
 print("records found: " + str(counter) + " processing time: "+ str(round(time.time()-start_time,2))+" s")
